a976e4b286b1a442623f06ee4cfdaf71ddf2291e
Tristan Bart <t.bart@openium.fr>
2026-04-03 16:43:44 +0200

    fix notification hour (7 -> 17)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.tbart.blackjack"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
